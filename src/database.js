46815954d964300227b88c6d0aabbb6ca3327650
Shreya Mohan <shreyamohan.2nov@gmail.com>
2024-10-22 22:49:52 +0530

    changes

--- src/database.js
+++ src/database.js
@@ -33,3 +33,8 @@ export default connect;
 // git checkout -b shreya
 
 // git push origin branch
+
+// see, there are changes that need to be pushed, but chnages maine kiye shreya mein, but push karne ka try kar rha hu main mein, to vo nhi kar rha kuch bhii
+// kyuu main mein agar hum push akre toh vo tabtak nahi hoga jabtak vo main branch current* nahi bann jaati? haan jabtak tum main per kaam nhi kar rhe tab tak vo push bhi nhi karne dega, agar push karne deta vo to log aake apni branch create karte aur kuch ulta seedha seedhe main mein push kar dete achaa okayy //let this comment be here?
+
+// so now I've pushed my changes I can see them here, but see magic
